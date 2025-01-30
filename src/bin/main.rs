8d4a0005e3f7359e82bd0a1c40e9f6163beff8b5
Antoine Zen-Ruffinen <antoine.zen@gmail.com>
2025-01-30 23:02:44 +0100

    Switch to more mature "gc9a01-rs" display driver.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use defmt::info;
+// ESP32 Hardware abstraction
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
 use esp_hal::gpio::{Level, Output};
@@ -12,37 +12,37 @@ use esp_hal::spi::{
 };
 use esp_hal::time::RateExtU32;
 
+// Generic hardware abstraction
 use embedded_hal_bus::spi::ExclusiveDevice;
-
-use embedded_hal::delay::DelayNs;
 
 // Embedded graphics
 use embedded_graphics::{
-    draw_target::DrawTarget,
     mono_font::{ascii::FONT_7X13, MonoTextStyle},
     prelude::*,
     text::Text,
 };
+// Screen driver
+use gc9a01::{prelude::*, Gc9a01, SPIDisplayInterface};
 
-use core::fmt::Write;
+// heap-less string buffer
 use heapless::String;
 
-// Screen driver
-use gc9a01::GC9A01;
-
+// Logging stuff
+use core::fmt::Write;
+use defmt::{error, info};
 use {defmt_rtt as _, esp_backtrace as _};
 
 extern crate alloc;
 
 #[main]
 fn main() -> ! {
-    // generator version: 0.2.2
-
+    // Get periferals from the hal.
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
     let mut delay = Delay::new();
 
+    // Split GPIO for peripherals blcok
     let sclk = peripherals.GPIO6;
     let mosi = peripherals.GPIO5;
     let cs = peripherals.GPIO7;
@@ -50,10 +50,11 @@ fn main() -> ! {
     let reset = peripherals.GPIO8;
     let bl = peripherals.GPIO9;
 
+    // Create SPI driver
     let spi = Spi::new(
         peripherals.SPI2,
         Config::default()
-            .with_frequency(10.MHz())
+            .with_frequency(50.MHz())
             .with_mode(Mode::_0),
     )
     .unwrap()
@@ -61,30 +62,43 @@ fn main() -> ! {
     .with_mosi(mosi);
     //.with_cs(cs);
 
+    // Create output drivers from GPIOs
     let rs_out = Output::new(rs, Level::High);
     let cs_out = Output::new(cs, Level::High);
     let mut bl_out = Output::new(bl, Level::Low);
-
     let mut reset_out = Output::new(reset, Level::High);
 
+    // Create SPI device and display interface adapter
     let display_dev = ExclusiveDevice::new_no_delay(spi, cs_out).unwrap();
-    let mut display = GC9A01::new(display_dev, rs_out);
+    let display_iface = SPIDisplayInterface::new(display_dev, rs_out);
 
-    // Reset display controller
-    delay.delay_ms(100);
-    reset_out.set_low();
-    delay.delay_ms(100);
-    reset_out.set_high();
-    delay.delay_ms(100);
+    // Create the display driver
+    let mut display = Gc9a01::new(
+        display_iface,
+        DisplayResolution240x240,
+        DisplayRotation::Rotate180,
+    )
+    .into_buffered_graphics();
 
+    if display.reset(&mut reset_out, &mut delay).is_err() {
+        error!("Display reset error");
+        loop {}
+    }
+    // Init and clear display
+    if display.init(&mut delay).is_err() {
+        error!("Display Init error");
+        loop {}
+    }
+    display.clear();
+    display.fill(0x0);
+    if display.flush().is_err() {
+        error!("Display flush error");
+    }
+
+    // Show must go on !
     bl_out.set_high();
 
-    display.setup();
-    delay.delay_ms(100);
-
-    display.clear(RgbColor::BLACK).unwrap();
-
-    let norm_style = MonoTextStyle::new(&FONT_7X13, RgbColor::CYAN);
+    let norm_style = MonoTextStyle::new(&FONT_7X13, RgbColor::RED);
 
     let mut buffer: String<16> = String::new();
 
@@ -95,6 +109,9 @@ fn main() -> ! {
         Text::new(&buffer, Point::new(100, 100), norm_style)
             .draw(&mut display)
             .unwrap();
+        if display.flush().is_err() {
+            error!("Display flush error");
+        }
     }
 
     loop {
